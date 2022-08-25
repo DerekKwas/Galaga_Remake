d61af94001bee6fc9d6a207508723e2d5fde1a89
Derek Kwasniewski <dsk42203@comcast.net>
2022-08-25 18:31:30 -0400

    - Cleaned up imports - Added constant variable for class - Removed needless class variables - Removed get_random_vel() - Overhauled enemy movement with seek(), update(), and generate_waypoints()

--- enemy.py
+++ enemy.py
@@ -1,49 +1,74 @@
-from lib2to3.pytree import Base
 from base_ship import Base_Ship
+import pygame as pg
 import random
+import math
+vec = pg.math.Vector2
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 600
+MAX_WAYPOINTS = 2
+DEFAULT_VEL = 0,0
+MAX_SPEED = 5
+MAX_FORCE = .1
+APPROACH_RADIUS = 200
 
 class Enemy(Base_Ship):
-    def __init__(self, x, y, health):
-        super().__init__(x, y, health)
-        print("NEW ENEMY CREATED!")
-        self.health = 5
-        self.canDamage = True
-        self.DEFAULT_VEL = 2
-        self.VELList = [(1,1), (0,1), (-1, 1)]
-        self.VELOptions = [(1,1), (-1, 1)]
-        self.VELIndex = 1
-        self.currentVEL = (0,1)
-        self.previousVEL = (-1,1)
-        self.counter = 0
-        self.COUNTER_MAX = 15
+    def __init__(self, WIN, x, y, health):
+        super().__init__(WIN, x, y, health)
 
-    def get_random_vel(self, WIN_WIDTH, WIN_HEIGHT):
-        if self.counter >= self.COUNTER_MAX:
-            self.counter = 0
-            self.VELIndex = random.randint(0, len(self.VELOptions) -1)
-            self.previousVEL = self.currentVEL
-            self.VELOptions.append(self.previousVEL)
-            self.currentVEL = self.VELOptions[self.VELIndex]
-            del self.VELOptions[self.VELIndex]
-        if self.y > WIN_HEIGHT:
-            self.update_location(self.x, 0)
-            self.canDamage = True
-            self.counter += 1
+        self.location = vec(x, y)
+        self.velocity = vec(DEFAULT_VEL)
+        self.acceleration = vec(0,0)
+
+        self.waypoints = []
+        self.generate_waypoints()
+        self.targetIndex = 0
+        self.target = self.waypoints[self.targetIndex]
+        
+    def seek(self):
+        # Steering force = desired velocity - current velocity
+        self.desired_velocity = (self.target - self.location).normalize() * MAX_SPEED
+        steer = (self.desired_velocity - self.velocity)
+        steerLen = math.sqrt((math.pow(steer[0], 2)) + (math.pow(steer[1], 2)))
+
+        if steerLen > MAX_FORCE:
+            steer.scale_to_length(MAX_FORCE)
+        return steer
+
+    def update(self):
+        self.desired_velocity = (self.target - self.location)
+        dist = math.sqrt((math.pow(self.desired_velocity[0], 2)) + (math.pow(self.desired_velocity[1], 2)))                                   # len(self.desired_velocity)
+        self.desired_velocity.normalize() * MAX_SPEED
+        if dist < APPROACH_RADIUS:
+            self.targetIndex += 1
+            self.target = self.waypoints[self.targetIndex]
         else:
-            self.update_location(self.x + (self.currentVEL[0] * self.DEFAULT_VEL), self.y + (self.currentVEL[1] * self.DEFAULT_VEL))
-            self.counter += 1
-            # print(f"Current: {self.currentVEL}\nLast: {self.previousVEL}\nOptions: {self.VELOptions}")
+            self.target = self.waypoints[self.targetIndex]
 
+        self.acc = self.seek()
 
+        # Equations of motion
+        self.velocity += self.acc
+        if len(self.velocity) > MAX_SPEED:
+            self.velocity.scale_to_length(MAX_SPEED)
+        self.location += self.velocity
+        if self.location.x > WIN_WIDTH:
+            self.location.x = 0
+        if self.location.x < 0:
+            self.location.x = WIN_WIDTH
+        if self.location.y > WIN_HEIGHT:
+            self.location.y = 0
+            self.targetIndex = 0
+        if self.location.y < 0:
+            self.location.y = WIN_HEIGHT
 
-
-
-
-
-        # ENEMY_VEL = self.VEL * random.choice((-1, 1))
-        # if self.y > WIN_HEIGHT:
-            # self.update_location(self.x, 0)
-            # self.canDamage = True
-        # else:
-            # self.update_location(self.x, self.y + self.VEL)
-        # self.update_location(self.x + ENEMY_VEL, self.y)
+    def generate_waypoints(self):
+        while len(self.waypoints) < MAX_WAYPOINTS: # 2 will be max generated waypoints for now
+            if len(self.waypoints) == 0:
+                point = (random.randrange(0, WIN_WIDTH), WIN_HEIGHT/2)
+                self.waypoints.append(point)
+            else:
+                point = (random.randrange(0, WIN_WIDTH), WIN_HEIGHT - self.HEIGHT)
+                self.waypoints.append(point)
+        point = (WIN_WIDTH/2, 1200)
+        self.waypoints.append(point)
