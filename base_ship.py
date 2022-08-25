d2bfe2afcda4df0ee7c9836db1d4e05ce3225442
Derek Kwasniewski <dsk42203@comcast.net>
2022-08-25 18:29:14 -0400

    - Added WIN & health to parameters

--- base_ship.py
+++ base_ship.py
@@ -1,12 +1,13 @@
-from pickle import NONE
 import pygame
 
 class Base_Ship:
-    def __init__(self, x, y, health):
+    def __init__(self, WIN, x, y, health):
         self.WIDTH, self.HEIGHT = 50, 50
         
+        self.WIN = WIN
         self.x = x
         self.y = y
+        self.health = health
         self.hitbox = pygame.Rect(x, y, self.WIDTH, self.HEIGHT)
 
     def update_location(self, x, y):
