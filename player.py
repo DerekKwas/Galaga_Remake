e424057124fd873beea2a4c5225f41a537ef9cd0
Derek Kwasniewski <dsk42203@comcast.net>
2022-08-25 18:31:46 -0400

    - Cleaned up __init__()

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from base_ship import Base_Ship
 
 class Player(Base_Ship):
-    def __init__(self, x, y, health):
-        super().__init__(x, y, health)
+    def __init__(self, WIN, x, y, health):
+        super().__init__(WIN, x, y, health)
         self.health = 10
