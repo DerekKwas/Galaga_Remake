59ee5c02e8501b978171c4e98a6cfbad0838bf1e
Derek Kwasniewski <dsk42203@comcast.net>
2022-08-28 12:45:05 -0400

    Removed health from parameter in __init__()

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from base_ship import Base_Ship
 
 class Player(Base_Ship):
-    def __init__(self, WIN, x, y, health):
-        super().__init__(WIN, x, y, health)
+    def __init__(self, WIN, x, y):
+        super().__init__(WIN, x, y)
         self.health = 10
